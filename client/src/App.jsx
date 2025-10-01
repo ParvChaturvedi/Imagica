a00f85c5cc5d90e6e2f9f8ca0df0baaa862fafe5
Parv Chaturvedi <parvchaturvedi2005@gmail.com>
2025-10-01 11:38:27 +0530

    Add backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,8 @@
 import React, { useContext } from 'react'
 import {Routes, Route} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
+import { Toaster } from 'react-hot-toast';
+import 'react-toastify/dist/ReactToastify.css';
 
 import Home from './pages/Home'
 import Result from './pages/Result'
@@ -7,6 +10,7 @@ import BuyCredit from './pages/BuyCredit'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Login from './components/Login'
+import ResetPassword from './components/ResetPassword';
 import { AppContext } from './context/AppContext'
 
 const App = () => {
@@ -17,12 +21,15 @@ const App = () => {
     <div className='px-4 sm:px-10 md:px-14 lg:px-28 
     min-h-screen bg-gradient-to-b from-teal-50 
     to-orange-50'>
+      <Toaster position="top-right" reverseOrder={false} />
+      <ToastContainer position='bottom-right'/>
       <Navbar/>
       {showLogin && <Login />}
       <Routes>
         <Route path='/' element={<Home/>}/>  
         <Route path='/result' element={<Result/>}/>
-        <Route path='/buy' element={<BuyCredit/>}/>  
+        <Route path='/buy' element={<BuyCredit/>}/> 
+        <Route path="/reset-password/:token" element={<ResetPassword />} />
       </Routes>  
       <Footer />   
     </div>
